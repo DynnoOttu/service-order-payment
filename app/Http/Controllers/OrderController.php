f693befa98aac284435c0b4631012555d1583b88
DynnoOttu <dynnoottu968@gmail.com>
2024-04-27 23:15:48 +0800

    API Create Orders Part 2

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,10 +2,69 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class OrderController extends Controller
 {
+
+    public function create(Request $request)
+    {
+        $user = $request->input('user');
+        $course = $request->input('course');
+
+        $order = Order::create([
+            'user_id' => $user['id'],
+            'course_id' => $course['id']
+        ]);
+
+        $transactionDetails = [
+            'order_id' => $order->id . '-' . Str::random(5),
+            'gross_amount' => $course['price']
+        ];
+
+        $itemDetails = [
+            [
+                'id' => $course['id'],
+                'price' => $course['price'],
+                'quantity' => 1,
+                'name' => $course['name'],
+                'brand' => 'BuildWithAngga',
+                'category' => 'Online Course'
+            ]
+        ];
+
+        $customerDetails = [
+            'first_name' => $user['name'],
+            'email' => $user['email']
+        ];
+
+        $midtransParams = [
+            'transaction_details' => $transactionDetails,
+            'item_details' => $itemDetails,
+            'customer_details' => $customerDetails
+        ];
+
+        $midtransSnapUrl = $this->getMidtransSnapUrl($midtransParams);
+
+        $order->snap_url = $midtransSnapUrl;
+
+        $order->metadata = [
+            'course_id' => $course['id'],
+            'course_price' => $course['price'],
+            'course_name' => $course['name'],
+            'course_thumbnail' => $course['thumbnail'],
+            'course_level' => $course['level']
+        ];
+
+        return $midtransSnapUrl;
+
+        // $order->save();
+
+        // return response()->json($order);
+    }
+
     private function getMidtransSnapUrl($params)
     {
         \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
