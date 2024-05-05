342012875527aa4cdba090d10544e1f50a015877
DynnoOttu <dynnoottu968@gmail.com>
2024-05-05 21:47:18 +0800

    API Create Premium Access

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -66,7 +66,6 @@ class WebhookController extends Controller
         } else if ($transactionStatus == 'pending') {
             $order->status = 'pending';
         }
-
         $logData = [
             'status' => $transactionStatus,
             'raw_response' => json_encode($data),
@@ -78,10 +77,10 @@ class WebhookController extends Controller
         $order->save();
 
         if ($order->status === 'success') {
-            // createPremiumAccess([
-            //     'user_id' => $order->user_id,
-            //     'course_id' => $order->course_id
-            // ]);
+            createPremiumAccess([
+                'user_id' => $order->user_id,
+                'course_id' => $order->course_id
+            ]);
         }
 
         return response()->json('Ok');
